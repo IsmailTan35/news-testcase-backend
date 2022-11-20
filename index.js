6f74342c75d11e4fff7817e43b79050c7a682c8c
IsmailTan35 <tan_tan.2000@hotmail.com>
2022-11-21 01:29:21 +0300

    .

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const apiController = require("./app_server/controller/apicontroller");
 const mongoDB = require("./app_server/model/mongoDb");
 const { httpServer } = require("./app_server/helper/helperProtocol");
 const dotenv = require("dotenv").config({ path: __dirname + "/./.env" });
+
 const app = express();
 
 mongoDB();
